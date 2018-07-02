2bd793870fa8cc9c5ec038258e2c48c2fe2a50b5
GEORGI-ASPIREV1\georgi <georgi.olentsenko@gmail.com>
2018-07-02 21:11:32 +0200

    Analysed the example

--- src/example.js
+++ src/example.js
@@ -13,17 +13,25 @@ module.exports = class Agent {
     offer(o){
         this.log(`${this.rounds} rounds left`);
         this.rounds--;
+		
+		// Decision whether to accept
         if (o)
         {
+			// Count the amount you'll get
             let sum = 0;
             for (let i = 0; i<o.length; i++)
                 sum += this.values[i]*o[i];
+			
+			// If the sum is at least half - accept
             if (sum>=this.total/2)
                 return;
         }
-        o = this.counts.slice();
+		
+		// If do no accept - counter offer
+        o = this.counts.slice(); // copy of the counts
         for (let i = 0; i<o.length; i++)
         {
+			// If there is no value to the item - discard
             if (!this.values[i])
                 o[i] = 0;
         }
