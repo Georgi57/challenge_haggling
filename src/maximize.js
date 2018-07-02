97699127dc89ccc7cc3c61b5222a7ec5dae9fe11
GEORGI-ASPIREV1\georgi <georgi.olentsenko@gmail.com>
2018-07-02 23:17:41 +0200

    Create opponents value list. Adjusted acception options.

--- src/maximize.js
+++ src/maximize.js
@@ -11,21 +11,29 @@ module.exports = class Agent {
         for (let i = 0; i<counts.length; i++)
             this.total += counts[i]*values[i];
 		
+		
 		// Sort the items by value, only once
 		this.my_values_ascending = []
 		// Fill it with item numbers
 		for (let i = 0; i < this.values.length; i++)
 			this.my_values_ascending.push(i);
-		
-		this.log(`Counts: ${this.counts}`);
-		this.log(`My values: ${this.values}`);
-		
+		// Sort my values
 		this.my_values_ascending.sort(function(a,b)
 		{
 			return values[a] - values[b];
 			
 		});
 		
+		
+		// Create opponent value list
+		this.opponents_values_descending = []
+		// Fill it with item numbers
+		for (let i = 0; i < this.values.length; i++)
+			this.opponents_values_descending.push(i);
+		
+		//TODO remove on submitting
+		this.log(`Counts: ${this.counts}`);
+		this.log(`My values: ${this.values}`);
 		this.log(this.my_values_ascending);
 		
 		this.last_offer = counts.slice(); // copy of the counts
@@ -35,7 +43,7 @@ module.exports = class Agent {
         this.log(`${this.rounds} rounds left`);
         this.rounds--;
 		
-		// Decision whether to accept
+		// Decision whether to accept (at least half + 1)
         if (o)
         {
 			// Count the amount you'll get
@@ -43,21 +51,28 @@ module.exports = class Agent {
             for (let i = 0; i<o.length; i++)
                 sum += this.values[i]*o[i];
 			
-			// If the sum is at least half - accept
-            if (sum>=this.total/2)
+			// If the sum is at least half plus one - accept
+            if (sum>=this.total/2 + 1)
                 return;
+			
+			// In the last round can content with half minus one
+			if (this.rounds == 0 && sum>=this.total/2 - 1)
+				return;
+			
+			// If this is an offer - analyze the opponents offer
         }
 		
 		// If do no accept - counter offer
         o = this.last_offer; // Take the last offer
+		
+		// Decrease the least valued item by one
         for (let i = 0; i<o.length; i++)
         {
-			// Decrease the least valued item by one
             if (o[this.my_values_ascending[i]] != 0)
 			{
                 o[this.my_values_ascending[i]]--;
 				this.log(`Offer: ${o}`);
-				return o
+				break;
 			}
         }
         return o;
